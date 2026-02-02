37e7297e6d7a56bbe54ebeeca3b4d8aab4b94db4
unknown <asyadeveloper@gmail.com>
2026-02-03 00:32:33 +0100

    Refined datasets + clear results

--- scripts/circuits/patching_algorithms.py
+++ scripts/circuits/patching_algorithms.py
@@ -77,6 +77,15 @@ class Patching:
         print(f"Clean logit diff: {self.clean_baseline:.4f}")
         print(f"Corrupted logit diff: {self.corrupted_baseline:.4f}")
 
+    def append_ticks(self, patch_metrics, prompt_number, is_clean=True):
+        assert(prompt_number < len(self.clean_tokens))
+        if is_clean:
+            ticks = self.clean_tokens[prompt_number].cpu()
+        else:
+            ticks = self.corrupted_tokens[prompt_number].cpu()
+        prompt_as_ticks = [f"{i}, {self.model.to_single_str_token(int(t))}" for i, t in enumerate(ticks)]
+        df = pd.DataFrame(patch_metrics.cpu(), columns=prompt_as_ticks)
+        return df
 
 class ActivationPatching(Patching):
     def __init__(self, model_name, clean_prompts, clean_answers, corrupted_prompts, corrupted_answers):
@@ -117,6 +126,7 @@ class ActivationPatching(Patching):
                 self.clean_baseline - self.corrupted_baseline
             )
 
+        # for batch..
         every_block_act_patch_result = layer_specific_algorithm(
             self.model, self.corrupted_tokens, self.clean_cache, __inner_logit_metric__)
 
